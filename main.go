dc604de883a9e22eb5d18ca34849743944e8066a
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2023-08-15 18:23:36 +0530

    feat: setting up twitter APIs

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"strings"
+	"time"
 
 	"github.com/SanjaySinghRajpoot/remote-crawler/config"
+	"github.com/SanjaySinghRajpoot/remote-crawler/models"
 	"github.com/SanjaySinghRajpoot/remote-crawler/routes"
+	"github.com/dghubble/oauth1"
 	"github.com/gin-gonic/gin"
-	"gopkg.in/robfig/cron.v2"
+	"github.com/gocolly/colly"
 )
 
 var cnt = 10
@@ -14,128 +19,129 @@ var cnt = 10
 // need a CRON Job for 24 hours set
 func runCronJobs() {
 
-	// c := colly.NewCollector()
+	c := colly.NewCollector()
 
-	// c.OnRequest(func(r *colly.Request) {
-	// 	fmt.Println("Visiting", r.URL)
-	// })
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
 
-	// tempjobs := make([]models.Job, 0)
+	tempjobs := make([]models.Job, 0)
 
-	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-	// 	link := e.Attr("href")
-	// 	// Print link
-	// 	url := fmt.Sprintf("https://himalayas.app%s", link)
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		url := fmt.Sprintf("https://himalayas.app%s", link)
 
-	// 	name := e.ChildText("h2.text-xl.font-medium.text-gray-900")
-	// 	if name != "" && cnt != 0 {
+		name := e.ChildText("h2.text-xl.font-medium.text-gray-900")
+		if name != "" && cnt != 0 {
 
-	// 		saveJob := models.Job{
-	// 			Name:        name,
-	// 			Description: "",
-	// 			URL:         url,
-	// 		}
+			saveJob := models.Job{
+				Name:        name,
+				Description: "",
+				URL:         url,
+			}
 
-	// 		fmt.Print(saveJob)
+			fmt.Print(saveJob)
 
-	// 		tempjobs = append(tempjobs, saveJob)
+			tempjobs = append(tempjobs, saveJob)
 
-	// 		cnt--
-	// 	}
-	// })
+			cnt--
+		}
+	})
 
-	// c.Visit("https://himalayas.app/jobs/developer")
+	c.Visit("https://himalayas.app/jobs/developer")
 
-	// for _, r := range tempjobs {
+	for _, r := range tempjobs {
 
-	// 	save := models.Job{
-	// 		Name:        r.Name,
-	// 		Description: r.Description,
-	// 		URL:         r.URL,
-	// 	}
+		save := models.Job{
+			Name:        r.Name,
+			Description: r.Description,
+			URL:         r.URL,
+		}
 
-	// 	result := config.DB.Create(&save)
+		result := config.DB.Create(&save)
 
-	// 	if result.Error != nil {
-	// 		fmt.Println("error")
-	// 		return
-	// 	}
-	// }
+		if result.Error != nil {
+			fmt.Println("error")
+			return
+		}
+	}
 
 	fmt.Println("himalayas website crawl")
 }
 
-func runHackerNews() {
+func sendTweeet(tweet string) {
 
-	// c := colly.NewCollector()
+	consumerKey := "Xco09OE6ivMarRZTaSxVXdoDX"
+	consumerSecret := "tFWo5bYLSRc5F2BXAhaMrA8qVO3MgAXRzQfhNmiEm2QPCgK1w8"
+	accessToken := "1690419507049975808-rtvJQhkVYAM9eLASFoTJVOS1VEirZs"
+	accessSecret := "JyC1RbZQq0ErQM6mX2XmjDuIejuXVq8bur0MNk0xf8S6V"
+	// prompt := os.Getenv("PROMPT")
 
-	// c.OnRequest(func(r *colly.Request) {
-	// 	fmt.Println("Visiting", r.URL)
-	// })
+	if consumerKey == "" || consumerSecret == "" || accessToken == "" || accessSecret == "" {
+		panic("Missing required environment variable")
+	}
 
-	// tempjobs := make([]models.Job, 0)
+	fetched := tweet
 
-	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-	// 	link := e.Attr("href")
-	// 	// Print link
-	// 	url := fmt.Sprintf("https://himalayas.app%s", link)
+	// From here on, Twitter POST API
+	config := oauth1.NewConfig(consumerKey, consumerSecret)
+	token := oauth1.NewToken(accessToken, accessSecret)
 
-	// 	name := e.ChildText("h2.text-xl.font-medium.text-gray-900")
-	// 	if name != "" && cnt != 0 {
+	httpClient := config.Client(oauth1.NoContext, token)
 
-	// 		saveJob := models.Job{
-	// 			Name:        name,
-	// 			Description: "",
-	// 			URL:         url,
-	// 		}
+	// Necessary - Lambdas timing out.
+	httpClient.Timeout = time.Second * 10
 
-	// 		fmt.Print(saveJob)
+	path := "https://api.twitter.com/2/tweets"
 
-	// 		tempjobs = append(tempjobs, saveJob)
+	body := fmt.Sprintf(`{"text": "%s"}`, fetched)
 
-	// 		cnt--
-	// 	}
-	// })
+	bodyReader := strings.NewReader(body)
 
-	// c.Visit("https://himalayas.app/jobs/developer")
+	response, err := httpClient.Post(path, "application/json", bodyReader)
 
-	// for _, r := range tempjobs {
+	if err != nil {
+		log.Fatalf("Error when posting to twitter: %v", err)
+	}
 
-	// 	save := models.Job{
-	// 		Name:        r.Name,
-	// 		Description: r.Description,
-	// 		URL:         r.URL,
-	// 	}
+	defer response.Body.Close()
+	log.Printf("Response was OK: %v", response)
 
-	// 	result := config.DB.Create(&save)
+	fmt.Println("tweet was succesfull")
+}
 
-	// 	if result.Error != nil {
-	// 		fmt.Println("error")
-	// 		return
-	// 	}
-	// }
+func getTweetFromDB() {
 
-	fmt.Println("hacker news crawler")
+	jobs := []models.Job{}
+	config.DB.Limit(3).Find(&jobs)
+
+	for _, job := range jobs {
+
+		// tweet := fmt.Sprintf("Name: %s \n , Description: %s, \n link: %s", job.Name, job.Description, job.URL)
+
+		tweet := fmt.Sprintf("Name: %s ", job.Name)
+
+		sendTweeet(tweet)
+	}
+
 }
 
 func main() {
 
 	config.Connect()
 
-	cronJob := cron.New()
+	// cronJob := cron.New()
 
-	cronJob.AddFunc("@every 1s", func() {
-		// can use go routines
-		runHackerNews()
-	})
+	// cronJob.AddFunc("@every 1s", func() {
+	// 	runCronJobs()
+	// })
 
-	cronJob.AddFunc("@every 1s", func() {
-		runCronJobs()
-	})
+	// cronJob.Start()
 
-	cronJob.Start()
+	// fmt.Scanln()
 
-	fmt.Scanln()
+	getTweetFromDB()
 
 	// starting the golang server
 	router := gin.New()
